586004dec66eeeaaba712cc0e205a1d130c929b0
YohannBlack <blackburnyohann@gmail.com>
2025-06-01 18:13:27 +0200

    changed main for new functions and classes

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 import os
 
-from resume_parser.resume_parser import extract_text_from_pdf
+from resume_parser.resume_parser import ResumeParser
 
 
 def main():
     cwd = os.getcwd()
     path_to_pdf = os.path.join(cwd, "src/data/resume_ligne.pdf")
-    pdf_text = extract_text_from_pdf(path_to_pdf)
-    print(pdf_text)
+    resume_parser = ResumeParser(ner_model_name="manishiitg/resume-ner")
+
+    if os.path.exists(path_to_pdf):
+        pdf_results = resume_parser.parse(path_to_pdf)
+        if pdf_results:
+            print("\n--- Parsed PDF Results ---")
+            for key, value in pdf_results["entities"].items():
+                print(f"{key}: {value}")
 
 if __name__ == "__main__":
     main()
